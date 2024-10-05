d941ef516b7fbfd20054b4138e87539114d0c544
Tom Zhang <tommyawesome08@gmail.com>
2024-10-05 16:41:08 -0400

    got rid of the overflow on the body tag 💀

--- veronomics/veronomics_frontend/src/Spinner.tsx
+++ veronomics/veronomics_frontend/src/Spinner.tsx
@@ -2,12 +2,14 @@ import "./Spinner.css";
 
 const Spinner = () => {
   return (
-    <div className="orbit-container">
-      <div className="planet"></div>
-      <div className="orbit-ring ring1"></div>
-      <div className="orbit-ring ring2"></div>
-      <div className="orbit-object object1"></div>
-      <div className="orbit-object object2"></div>
+    <div className="spinner-body">
+      <div className="orbit-container">
+        <div className="planet"></div>
+        <div className="orbit-ring ring1"></div>
+        <div className="orbit-ring ring2"></div>
+        <div className="orbit-object object1"></div>
+        <div className="orbit-object object2"></div>
+      </div>
     </div>
   );
 };
